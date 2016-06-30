e3c9c9d9a4a91e5463c057d802f72d5259ae5c15
Robby Kim <robert.st.kim@gmail.com>
2016-06-30 14:43:56 -0400

    started the delete request up to getting for loop running

--- server.js
+++ server.js
@@ -1,48 +1,59 @@
-var express = require( 'express' );
-var bodyParser = require( 'body-parser' );
+var express = require('express');
+var bodyParser = require('body-parser');
 // CONSTRUCTOR
-var Storage = function() {
+var Storage = function () {
   this.items = [];
   this.id = 0;
 };
 
 // ADD METHOD for CONSTRUCTOR
-Storage.prototype.add = function( name ) {
+Storage.prototype.add = function (name) {
   var item = {
     name: name,
     id: this.id
   };
-  this.items.push( item );
+  this.items.push(item);
   this.id += 1;
   return item;
 };
 
 // ADDING items to list
 var storage = new Storage();
-storage.add( 'Broad beans' );
-storage.add( 'Tomatoes' );
-storage.add( 'Peppers' );
+storage.add('Broad beans');
+storage.add('Tomatoes');
+storage.add('Peppers');
 
 // USE CONSTRUCTORS to create OBJECTS
 var app = express();
 var jsonParser = bodyParser.json();
 
 // serves static files in public folder
-app.use( express.static( 'public' ) );
+app.use(express.static('public'));
 
 // localhost:8080/items
-app.get( '/items', function( req, res ) {
-  res.json( storage.items );
-} );
+app.get('/items', function (req, res) {
+  res.json(storage.items);
+});
 
 // Data is formatted and parsed through body-parser (JSON format)
-app.post( '/items', jsonParser, function( req, res ) {
-  if ( !req.body ) {
-    return res.sendStatus( 400 );
+app.post('/items', jsonParser, function (req, res) {
+  if (!req.body) {
+    return res.sendStatus(400);
   }
 
-  var item = storage.add( req.body.name );
-  res.status( 201 ).json( item );
-} );
+  var item = storage.add(req.body.name);
+  res.status(201).json(item);
+});
 
-app.listen( process.env.PORT || 8080 );
+app.delete('/items/:id', function (req, res) {
+  var id = parseInt(req.params.id);
+  for (var i = 0; i < storage.items.length; i++) {
+    if (storage.items[i].id === id) {
+      console.log('delete');
+      return;
+    }
+  }
+  console.log('nothing');
+});
+
+app.listen(process.env.PORT || 8080);
