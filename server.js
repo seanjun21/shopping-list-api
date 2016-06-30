bd790d2939d28e40d51ba595ee808768476d0608
Sean Jun <seanjun21@gmail.com>
2016-06-30 09:43:05 -0700

    Added POST endpoint

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
-var express = require('express');
-
+var express = require( 'express' );
+var bodyParser = require( 'body-parser' );
 // CONSTRUCTOR
-var Storage = function () {
+var Storage = function() {
   this.items = [];
   this.id = 0;
-}
+};
 
 // ADD METHOD for CONSTRUCTOR
-Storage.prototype.add = function (name) {
+Storage.prototype.add = function( name ) {
   var item = {
     name: name,
     id: this.id
   };
-  this.items.push(item);
+  this.items.push( item );
   this.id += 1;
   return item;
 };
 
 // ADDING items to list
 var storage = new Storage();
-storage.add('Broad beans');
-storage.add('Tomatoes');
-storage.add('Peppers');
+storage.add( 'Broad beans' );
+storage.add( 'Tomatoes' );
+storage.add( 'Peppers' );
 
+// USE CONSTRUCTORS to create OBJECTS
 var app = express();
+var jsonParser = bodyParser.json();
+
 // serves static files in public folder
-app.use(express.static('public'));
+app.use( express.static( 'public' ) );
 
 // localhost:8080/items
-app.get('/items', function (req, res) {
-  res.json(storage.items);
-});
+app.get( '/items', function( req, res ) {
+  res.json( storage.items );
+} );
 
-app.listen(process.env.PORT || 8080);
+// Data is formatted and parsed through body-parser (JSON format)
+app.post( '/items', jsonParser, function( req, res ) {
+  if ( !req.body ) {
+    return res.sendStatus( 400 );
+  }
+
+  var item = storage.add( req.body.name );
+  res.status( 201 ).json( item );
+} );
+
+app.listen( process.env.PORT || 8080 );
